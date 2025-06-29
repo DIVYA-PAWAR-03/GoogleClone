7f8d0292d7f2995f8ca9013a2a251d1771b1469c
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-06-29 22:28:55 +0530

    made Google search website functional

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './style.css'
-alert('hii')
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+   
+    const searchInput = document.getElementById("txtSearch");
+    const searchButton = document.getElementById("btnGoogleSearch");
+
+   const searchGoogle = () => {
+    const query = searchInput.value;
+    if(query){
+        const url = `https://www.google.com/search?q=${query}`
+        window.location.href = url;
+    }
+    else{
+        alert('Please enter a search term')
+    }
+   };
+   searchButton.addEventListener('click', searchGoogle);
+   searchInput.addEventListener('keypress', (event)=>{
+    if(event.key === 'Enter'){
+        searchGoogle();
+    }
+   })
+});
